2e0d97bb74eb4b367d0faad9a1b43984f4453dc3
Anelise <anypteixeira@gmail.com>
2021-11-17 19:01:11 -0300

    javascript1

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var apresentacao = "Ola meu nome é";
+var nome = " Anelise";
+var anos = 14;
+
+var mensagem = apresentacao + nome + "eu tenho " + anos + " anos";
+
+console.log(mensagem);
+
+//
+
+var idade = 50 + 47 + 22 + 14 - 33;
+
+console.log(idade);
